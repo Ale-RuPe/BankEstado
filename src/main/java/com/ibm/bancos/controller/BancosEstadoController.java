c215ed22198b11469e0652d71924101cb477e5f8
Alejandro Ruiz Perez <alejandro.perez1@ibm.com>
2020-04-20 11:57:06 -0500

    validador

--- src/main/java/com/ibm/bancos/controller/BancosEstadoController.java
+++ src/main/java/com/ibm/bancos/controller/BancosEstadoController.java
@@ -3,15 +3,18 @@ package com.ibm.bancos.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ibm.bancos.model.BancoModel;
 import com.ibm.bancos.service.BancosEstadoService;
+import com.ibm.bancos.validator.Validator;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,13 +22,22 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 public class BancosEstadoController {
 	@Autowired
+	Validator validator;
+	
+	@Autowired
 	BancosEstadoService service;
 	
+	
 	@GetMapping("${controller.uri}")
-	public ResponseEntity<List<BancoModel>> getBancos( 
+	public ResponseEntity<List<BancoModel>> getBancos(@RequestHeader HttpHeaders httpHeaders,
 			@PathVariable String state) throws MissingServletRequestParameterException {
 		
-		log.info("Controller receiving PathVariable: {}", state);
+		log.info("Headers {}", httpHeaders.toString());
+		validator.validateHeaders(httpHeaders.toSingleValueMap());
+		
+		log.info("PathVariable: {}", state);
+		validator.validateParam(state);
+		
 		List<BancoModel> bancos = service.findBancos(state);
 		log.info("Returning response: {}", bancos.size());
 		
